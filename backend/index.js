e9dba7b6e52d766cd46058188d6ff6cd15832e97
VISHALSHARMAVS <vishalsharmakt143@gamil.com>
2024-08-06 03:53:28 +0530

    user model

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,10 @@ const cors = require('cors');
 const connectionDB = require('./db.js');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(bodyParser.json())
+
 
 const PORT= process.env.PORT || 3000;
 connectionDB().then(()=>{
---
+++ backend/models/user.model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+     name :{
+        type:String,
+        required:[true,'Please add a name'],
+     },
+     email:{
+        type:String,
+        required:true,
+        unique:true,
+        trim : true,
+        match:[
+            /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,"Please enter an valid email"
+        ]
+     },
+     password:{
+        type:String,
+        required:[true,'Please add a password'],
+        minLength:[8,"password must be up to 8 characters"],
+        maxLength:[18,"password must not be more than 18 characters"]
+     },
+     photo:{
+        type:String,
+        required:[true,'Please add a Photo'],
+        default:""
+     },
+     phone:{
+        type:String,
+        default:"+91"
+     },
+     bio:{
+        type:String,
+        maxLength:[250,"Bio must not be more than 250 characters"],
+        default:"bio"
+
+     }
+
+},{timestamps:true})
+
+const User = mongoose.model('User',userSchema);
+module.exports = User
