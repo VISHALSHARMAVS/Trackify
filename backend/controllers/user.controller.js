4c11bdc02c05b8bf972866edc298772acafcb4d6
VISHALSHARMAVS <vishalsharmakt143@gamil.com>
2024-08-06 23:19:25 +0530

    register router

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,42 @@
+const User = require('../models/user.model.js')
+
+const registerUser = async(req,res)=>{
+    const {name,email,password}= req.body;
+    if (!name || !email || !password) {
+        res.status(400);
+        throw new Error("Please fill all required fields");
+
+    }
+    if (password.length<8) {
+        res.status(400);
+        throw new Error("Password shold be 8 characters");   
+    }
+    try {
+        
+      const userExists=  await User.findOne({email});
+      if (userExists) {
+        res.status(400);
+        throw new Error("Email has already been registered");
+      }
+
+      const user = await User.create({
+        name,email,password
+      })
+      if (user) {
+          const {_id,name,email,photo,bio,phone}= req.body;
+        res.status(201).json({
+            _id,name,email,photo,bio,phone
+        })
+    }
+    else {
+        res.status(400);
+        throw new Error("Invalid user data")
+    }
+
+      
+    } catch (error) {
+        
+    }
+}
+
+module.exports = {registerUser}
--- backend/index.js
+++ backend/index.js
@@ -2,21 +2,31 @@ const dotenv = require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const userRoute = require('./routes/user.route.js');
 const connectionDB = require('./db.js');
+const errorHandling = require('./middleware/errorHandling.js');
 
 const app = express();
+
+// Middleware
+app.use(cors()); // Enable CORS
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+// Routes
+app.use('/api/user', userRoute);
 
-const PORT= process.env.PORT || 3000;
-connectionDB().then(()=>{
-    app.listen(PORT,()=>{
-        console.log(`server running on port ${PORT}`);
-        
-    })
-}).catch((error)=>{
+// Error handling middleware
+app.use(errorHandling);
+
+const PORT = process.env.PORT || 3000;
+
+// Database connection and server start
+connectionDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}).catch((error) => {
     console.log(error);
-    
-})
+});
